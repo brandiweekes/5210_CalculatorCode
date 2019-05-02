829c8bf1426305bdb657c1fb4b75624f71ea2dec
Harshal Vadnere <harshal10@gmail.com>
2019-05-01 18:26:55 -0700

    add cat years calc tests

--- UnitTests/Models/AgeCalculatorUnitTests.cs
+++ UnitTests/Models/AgeCalculatorUnitTests.cs
@@ -13,7 +13,7 @@ namespace UnitTests.Models
     public class AgeCalculatorUnitTests
     {
         [Test]
-        public void TestMethod()
+        public void AgeCalc_valid_input_should_pass()
         {
             //Arrange
             var model = new CalculatorCode.Models.AgeCalculator();
@@ -28,6 +28,22 @@ namespace UnitTests.Models
             Assert.AreEqual(actual.Days, expected.Days, "Some useful error message");
             
             //Reset NA
+        }
+
+        [Test]
+        public void AgeCalc_valid_input_year_in_future_should_pass()
+        {
+            //Arrange
+            var model = new CalculatorCode.Models.AgeCalculator();
+            DateTime BirthDate = new DateTime(2028, 3, 28, 0, 0, 0);
+            model.InputOne = BirthDate;
+            TimeSpan expected = DateTime.Now - model.InputOne;
+
+            //Act
+            var actual = model.CalculateAge();
+
+            //Assert note that Checking only for days accuracy
+            Assert.AreEqual(actual.Days, expected.Days, "Some useful error message");
         }
     }
 }
---
+++ UnitTests/Models/CatYearsCalculatorUnitTests.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CalculatorCode.Models;
+
+namespace UnitTests.Models
+{
+    [TestFixture]
+    public class CatYearsCalculatorUnitTests
+    {
+        [Test]
+        public void CatYearsCalc_valid_input_should_pass()
+        {
+            //Arrange
+            var model = new CalculatorCode.Models.CatYearsCalculator();
+            model.InputOne = 12;
+            var expected = model.InputOne * 7;
+
+            //Act
+            var actual = model.CalculateResult();
+            
+            //Assert note that Checking only for days accuracy
+            Assert.AreEqual(actual, expected, "Some useful error message");
+        }
+
+        [Test]
+        public void CatYearCalc_valid_input_neg_year_should_pass()
+        {
+            //Arrange
+            var model = new CalculatorCode.Models.CatYearsCalculator();
+            model.InputOne = -22;
+            var expected = model.InputOne * 7;
+
+            //Act
+            var actual = model.CalculateResult();
+
+            //Assert note that Checking only for days accuracy
+            Assert.AreEqual(actual, expected, "Some useful error message");
+        }
+    }
+}
