5b56bfe9383ea6101f64882882c42044e74d2b41
brandiweekes <brandiweekes@gmail.com>
2019-05-31 14:37:32 -0700

    binary subtraction unit tests

--- UnitTests/Models/BinaryValueUnitTests.cs
+++ UnitTests/Models/BinaryValueUnitTests.cs
@@ -12,6 +12,7 @@ namespace UnitTests.Models
     [TestFixture]
     public class BinaryValueUnitTests
     {
+        #region Sum
         [Test]
         public void BinaryValue_SumInBinaryFromStrings_inputOne_11111111_inputTwo_00000000_should_return_valid_binary()
         {
@@ -131,5 +132,128 @@ namespace UnitTests.Models
             // Assert
             Assert.AreEqual(expectedDecimal, actualDecimal, "converting addition to decimal: 2147483648");
         }
+        #endregion
+
+        #region Subtraction
+        [Test]
+        public void BinaryValue_SubtractionInBinaryFromStrings_inputOne_11111111_inputTwo_00000000_should_return_valid_binary()
+        {
+            // Arrange            
+            var inputOne = "11111111";
+            var inputTwo = "00000000";
+            var expectedBinary = "11111111";
+
+            // Act
+            var actualBinary = BinaryValue.SubtractionInBinaryFromStrings(inputOne, inputTwo);
+
+            // Assert
+            Assert.AreEqual(expectedBinary, actualBinary, "converting subtraction to binary: 11111111");
+        }
+
+        [Test]
+        public void BinaryValue_SubtractionInBinaryFromStrings_inputOne_10000000000000000000000000000000_inputTwo_1_should_return_valid_binary()
+        {
+            // Arrange
+            var inputOne = "10000000000000000000000000000000";
+            var inputTwo = "1";
+            var expectedBinary = "1111111111111111111111111111111";
+
+            // Act
+            var actualBinary = BinaryValue.SubtractionInBinaryFromStrings(inputOne, inputTwo);
+
+            // Assert
+            Assert.AreEqual(expectedBinary, actualBinary, "converting subtraction to binary: 1111111111111111111111111111111");
+        }
+
+        [Test]
+        public void BinaryValue_SubtractionInDecimalFromStrings_inputOne_11111111_inputTwo_00000000_should_return_valid_decimal()
+        {
+            // Arrange
+            var inputOne = "11111111";
+            var inputTwo = "00000000";
+            var expectedDecimal = 255;
+
+            // Act
+            var actualDecimal = BinaryValue.SubtractionInDecimalFromStrings(inputOne, inputTwo);
+
+            // Assert
+            Assert.AreEqual(expectedDecimal, actualDecimal, "converting subtraction to decimal: 255");
+        }
+
+        [Test]
+        public void BinaryValue_SubtractionInDecimalFromStrings_inputOne_10000000000000000000000000000001_inputTwo_1_should_return_valid_binary()
+        {
+            // Arrange
+            var inputOne = "10000000000000000000000000000001";
+            var inputTwo = "1";
+            var expectedDecimal = "2147483648";
+
+            // Act
+            var actualDecimal = BinaryValue.SubtractionInDecimalFromStrings(inputOne, inputTwo);
+
+            // Assert
+            Assert.AreEqual(expectedDecimal, actualDecimal, "converting subtraction to binary: 2147483648");
+        }
+
+        //[Test]
+        //public void BinaryValue_SubtractionInBinary_inputOne_11111111_inputTwo_00000000_should_return_valid_binary()
+        //{
+        //    // Arrange
+        //    var inputOne = new BinaryValue("11111111");
+        //    var inputTwo = new BinaryValue("00000000");
+        //    var expectedBinary = "11111111";
+
+        //    // Act
+        //    var actualBinary = BinaryValue.SubtractionInBinary(inputOne, inputTwo);
+
+        //    // Assert
+        //    Assert.AreEqual(expectedBinary, actualBinary, "converting subtraction to binary: 11111111");
+        //}
+
+        //[Test]
+        //public void BinaryValue_SubtractionInBinary_inputOne_10000000000000000000000000000000_inputTwo_1_should_return_valid_binary()
+        //{
+        //    // Arrange
+        //    var inputOne = new BinaryValue("10000000000000000000000000000000");
+        //    var inputTwo = new BinaryValue("1");
+        //    var expectedBinary = "1111111111111111111111111111111";
+
+        //    // Act
+        //    var actualBinary = BinaryValue.SubtractionInBinary(inputOne, inputTwo);
+
+        //    // Assert
+        //    Assert.AreEqual(expectedBinary, actualBinary, "converting subtraction to binary: 10000000000000000000000000000000");
+        //}
+
+        //[Test]
+        //public void BinaryValue_SubtractionInDecimal_inputOne_11111111_inputTwo_00000000_should_return_valid_decimal()
+        //{
+        //    // Arrange
+        //    var inputOne = new BinaryValue("11111111");
+        //    var inputTwo = new BinaryValue("00000000");
+        //    var expectedDecimal = 255;
+
+        //    // Act
+        //    var actualDecimal = BinaryValue.SubtractionInDecimal(inputOne, inputTwo);
+
+        //    // Assert
+        //    Assert.AreEqual(expectedDecimal, actualDecimal, "converting subtraction to decimal: 255");
+        //}
+
+        //[Test]
+        //public void BinaryValue_SubtractionInDecimal_inputOne_10000000000000000000000000000001_inputTwo_1_should_return_valid_decimal()
+        //{
+        //    // Arrange
+        //    var inputOne = new BinaryValue("10000000000000000000000000000001");
+        //    var inputTwo = new BinaryValue("1");
+        //    var expectedDecimal = 2147483648;
+
+        //    // Act
+        //    var actualDecimal = BinaryValue.SubtractionInDecimal(inputOne, inputTwo);
+
+        //    // Assert
+        //    Assert.AreEqual(expectedDecimal, actualDecimal, "converting subtraction to decimal: 2147483648");
+        //}
+        #endregion
     }
 }
